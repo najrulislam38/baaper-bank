2935b9189db4bd9f414ab9237eb835bb3c2f963b
Najrul <mdnajrulislam.4738@gmail.com>
2023-08-15 15:21:28 +0600

    Bapper Bank projert completed.

---
+++ js/deposit.js
@@ -0,0 +1,32 @@
+// step-1: add event listener to ta deposit button.
+document.getElementById("btn-deposit").addEventListener('click', function(){
+    // step-2: get the deposit amount form the deposit input field.
+    // for the input field .value to the value inside the input field.
+    const depositField = document.getElementById('deposit-field');
+    const depositAmountString = depositField.value;
+    const depositAmount = parseFloat(depositAmountString)
+
+    if (depositAmountString <= 0 || depositAmountString === "") {
+       return alert ("please provide me Deposit Amount")
+    }
+    
+    // step-3: get the current deposit amount total
+    // for no-input (element other than input and textarea) use innerText get the text.
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    
+    // step-4: add numbers to set the total deposit.
+    depositTotalElement.innerText = previousDepositTotal + depositAmount;
+
+    // step-5: get total current balance .
+    const balanceTotalElement = document.getElementById('balance-total');
+    const balanceTotalString = balanceTotalElement.innerText;
+    const balanceTotal = parseFloat(balanceTotalString);
+
+    // step-6: add numbers to set the total balance.
+    balanceTotalElement.innerText = balanceTotal + depositAmount;
+
+    // step-7: clear the deposit field
+    depositField.value = "";
+})
---
+++ js/withdraw.js
@@ -0,0 +1,46 @@
+/*
+1. add event handler with the withdraw button.
+2. get the amount from the withdraw input field.
+2-5. also make sure to convert the into number.
+3. get previous withdrew total
+4. calculate the withdraw amount
+4-5. set the withdraw total
+5. get the previous balance total
+6. calculate the new balance total
+7. clear the input field.
+*/
+
+// step-1
+document.getElementById("btn-withdrew").addEventListener("click", function () {
+  // step 2
+  const withdrawField = document.getElementById("withdraw-field");
+  const newWithdrawAmountString = withdrawField.value;
+  const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+  // step 3
+  const withdrawTotalElement = document.getElementById("withdraw-total");
+  const previousWithdrawTotalString = withdrawTotalElement.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+  // step 5
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  
+    // step 7
+    withdrawField.value = "";
+
+  if (newWithdrawAmount <= 0 || newWithdrawAmountString === "") {
+    return alert("please provide me Withdraw Amount");
+  } else if (newWithdrawAmount > previousBalanceTotal) {
+    return alert("You cant withdraw that much");
+  } else {
+    // step 4
+    const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = currentWithdrawTotal;
+
+    // step 6
+    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = currentBalanceTotal;
+  }
+});
